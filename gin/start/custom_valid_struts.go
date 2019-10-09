ae54cc3bdda847c5e216e70fe7593eb6f67bd1a5
Wisn <wuyishun_kmk@outlook.com>
2019-10-09 18:33:16 +0800

    update

--- gin/start/custom_valid_struts.go
+++ gin/start/custom_valid_struts.go
@@ -6,21 +6,25 @@ import (
 )
 
 type customValidRequest struct {
-	CheckIn  time.Time `form:"check_in" binding:"required" time_format:"2006-01-02"`
-	CheckOut time.Time `form:"check_out" binding:"required" time_format:"2006-01-02"`
+	//time_format:"2006-01-02"
+	CheckIn  *time.Time `form:"check_in" binding:"required"`
+	CheckOut *time.Time `form:"check_out" binding:"required"`
 }
 
 func main() {
 	g := gin.Default()
-	g.GET("/test", func(context *gin.Context) {
+	g.POST("/test", func(context *gin.Context) {
 		var customValidRequest customValidRequest
 		if error := context.ShouldBind(&customValidRequest); error != nil {
+			//bytes,_:=ioutil.ReadAll(context.Request.Body)
+			//fmt.Println(string(bytes))
 			context.JSON(500, gin.H{
 				"error": error.Error(),
 			})
 			context.Abort()
 			return
 		}
+
 		context.JSON(200, gin.H{
 			"message": "OK",
 		})
